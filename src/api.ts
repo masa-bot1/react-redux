d1ac9c3298a952953324569872ce02eb34aaa65a
masa-bot1 <bdyht515@yahoo.co.jp>
2022-02-12 01:33:04 +0900

    actionのユニットテスト

--- src/api.ts
+++ src/api.ts
@@ -37,12 +37,12 @@ export type ReqAndRes = {
 
   'GET /v1/cardsOrder': {
     req: null
-    res: Record<string, CardID | ColumnID>
+    res: Record<string, CardID | ColumnID | null>
   }
 
   'PATCH /v1/cardsOrder': {
-    req: Record<string, CardID | ColumnID>
-    res: Record<string, CardID | ColumnID>
+    req: Record<string, CardID | ColumnID | null>
+    res: Record<string, CardID | ColumnID | null>
   }
 }
 
